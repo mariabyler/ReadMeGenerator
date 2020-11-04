eb3f5da9780c45cdd390ed88bf251b2d6f104633
Maria Byler <mariabyler98@gmail.com>
2020-11-03 23:38:57 -0500

    added markdown js

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,7 +1,32 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
-
+  return `
+# Project title ${data.Title}
+## Developer Profile
+[This Link] (https://github.com/${data.username})
+### Table of Contents
+*[Contact](#Contact)
+*[Description](#Description)
+*[Installation](#Installation)
+*[Usage](#Usage)
+*[License](#License)
+*[Contributor](#Contributor)
+*[Test](#Test)
+## Contact 
+${data.Email}
+## Description 
+${data.Description}
+## Installation 
+${data.Installation}
+## Usage 
+${data.Usage}
+## License 
+${data.License} 
+![GitHub license](https://img.shields.io/badge/license-${data.License}-blue.svg)
+## Contributor 
+${data.Contributor}
+## Test
+${data.Test}
 `;
 }
 
